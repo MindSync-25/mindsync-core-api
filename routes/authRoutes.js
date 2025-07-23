8a5a2acee881590b10f40968c6f6d278ce51cc90
MindSync-25 <anil.bh.oct.98@gmail.com>
2025-07-22 23:24:05 -0400

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { register, googleLogin, appleLogin } = require('../controllers/authController');
+const { register, login, googleLogin, appleLogin } = require('../controllers/authController');
 
 // POST /api/auth/register
 router.post('/register', register);
+router.post('/login',    login);
 router.post('/google', googleLogin);
 router.post('/apple', appleLogin);
 
